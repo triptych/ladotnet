6371a638c8cc4ff0dd99f30c11e7c9e92ac0cb16
Andrew Wooldridge <triptych@gmail.com>
2021-04-04 17:35:00 -0700

    social stuff

--- src/lib/sections/header/Header.svelte
+++ src/lib/sections/header/Header.svelte
@@ -1,5 +1,9 @@
+<script>
+    import Social from '$lib/sections/social/Social.svelte';    
+</script>
 <div class="la-header">
     <h1><img src="law002.png" alt="lisa-anne-wooldridge"/></h1>
+    <Social/>
 </div>
 <style>
 .la-header {
---
+++ src/lib/sections/social/Social.svelte
@@ -0,0 +1,20 @@
+
+<script>
+     //import "../../../global.scss";
+</script>
+<div class="la-social">
+	<ul style="display:flex">
+		<li style="margin:8px">
+            <span class="icon has-text-info">
+                <i class="fab fa-facebook-square"></i>
+            </span>
+		</li>
+        <li style="margin:8px">
+            <span class="icon has-text-info">
+                <i class="fas fa-envelope-square"></i>
+            </span>
+		</li>
+	</ul>
+
+    
+</div>
