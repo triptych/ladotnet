cf775fc6a6ca6827efefb731cfb193a36aea59bd
Andrew Wooldridge <triptych@gmail.com>
2021-04-11 18:33:45 -0700

    social links

--- src/lib/sections/social/Social.svelte
+++ src/lib/sections/social/Social.svelte
@@ -1,20 +1,60 @@
-
 <script>
-     //import "../../../global.scss";
+	//import "../../../global.scss";
 </script>
+
 <div class="la-social">
 	<ul style="display:flex">
-		<li style="margin:8px">
-            <span class="icon has-text-info">
-                <i class="fab fa-facebook-square"></i>
-            </span>
+		<li>
+			<a href="https://www.facebook.com/LisaAnneIlluminated/">
+				<span class="icon has-text-info">
+					<i class="fab fa-facebook-square" />
+				</span></a
+			>
 		</li>
-        <li style="margin:8px">
-            <span class="icon has-text-info">
-                <i class="fas fa-envelope-square"></i>
-            </span>
+		<li>
+			<a href="lisaannebooks@gmail.com">
+				<span class="icon has-text-info">
+					<i class="fas fa-envelope-square" />
+				</span></a
+			>
+		</li>
+		<li>
+			<a href="https://www.instagram.com/lisaannebooks/">
+				<span class="icon has-text-info">
+					<i class="fab fa-instagram-square" />
+				</span>
+			</a>
+		</li>
+        <li>
+			<a href="https://lisaannebooks1/">
+				<span class="icon has-text-info">
+					<i class="fab fa-twitter-square" />
+				</span>
+			</a>
+		</li>
+        <li>
+			<a href="https://www.pinterest.com/1lisaanne">
+				<span class="icon has-text-info">
+					<i class="fab fa-pinterest-square" />
+				</span>
+			</a>
+		</li>
+        <li>
+			<a href="https://www.amazon.com/Lisa-Anne-Wooldridge/e/B08BF3NWNG">
+				<span class="icon has-text-info">
+					<i class="fab fa-amazon" />
+				</span>
+			</a>
 		</li>
 	</ul>
-
-    
 </div>
+
+<style lang="scss">
+	.la-social {
+        position: absolute;
+        right: 8px;
+        li {
+		cursor: pointer;
+		margin: 8px;
+	}}
+</style>
