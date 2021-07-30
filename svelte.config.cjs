7cb8d8e2b02d29e6e58329bb313ef184ab6278ca
Andrew Wooldridge <triptych@gmail.com>
2021-07-29 17:14:55 -0700

    fixing?

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -2,8 +2,6 @@ const sveltePreprocess = require('svelte-preprocess');
 const node = require('@sveltejs/adapter-node');
 const netlify = require('@sveltejs/adapter-netlify');
 const pkg = require('./package.json');
-//import adapter from '@sveltejs/adapter-static';
-//const static = require('@sveltejs/adapter-static');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
